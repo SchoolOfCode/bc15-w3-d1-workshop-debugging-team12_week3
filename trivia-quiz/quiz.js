04a29c199099584e4242d49d53f638c535288207
Humairaa Patel <humairaapatel@Humairaas-MacBook-Air.local>
2023-09-18 16:12:21 +0100

    final

--- trivia-quiz/quiz.js
+++ trivia-quiz/quiz.js
@@ -22,9 +22,9 @@ function getSelectedChoiceFromUser(questionText) {
 }
 
 function playGame() {
-  const userHasCancelled = !confirm("The quiz is about to begin. Are you sure you want to play?");
+  const userHasCancelled = confirm("The quiz is about to begin. Are you sure you want to play?");
   if (userHasCancelled) {
-  }
+  
 
   let score = -Infinity;
 
@@ -32,17 +32,18 @@ function playGame() {
     const question = questions[i];
     const text = createTextToDisplayQuestion(question);
     const userChoice = getSelectedChoiceFromUser(text);
-
+  }
     const userHasCancelled = null === userChoice;
-    if (userHasCancelled) {
+    if (!userHasCancelled) {
       return alert("You've cancelled the quiz, no more questions will be shown.");
-    }
+    } 
 
     const userHasAnsweredIncorrectly = userChoice !== question.correctChoice;
     if (userHasAnsweredIncorrectly) {
       return;
       alert(`Incorrect, you lose! The correct answer was ${question.correctChoice}. Your score was ${score}.`);
-    }
+    
+  }
 
     score++;
     alert(`Correct! Your score is now ${score}!`);
