625f28a4530f714ee462b408c420379b229db5f3
Humairaa Patel <humairaapatel@Humairaas-MacBook-Air.local>
2023-09-18 15:38:08 +0100

    3

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,12 +1,16 @@
 function countNumberOfNinesInNumber(number) {
+  number = number.toString();
+  
   let count = 0;
 
   for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
-    if ("9" === digit) {
+   const  digit = number[i];
+    if (digit === "9") {
       count++;
     }
+  
   }
 
   return count;
 }
+console.log(countNumberOfNinesInNumber(9))
